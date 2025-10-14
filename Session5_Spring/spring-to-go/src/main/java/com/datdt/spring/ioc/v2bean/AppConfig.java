bab8ce5375c279bb8b6237630a0e738159c4f9c1
dat dang <2imdanton139@gmail.com>
2025-10-14 09:58:07 +0700

    Get bean by name, cannot use the name which is different from method name in appconfig

--- Session5_Spring/spring-to-go/src/main/java/com/datdt/spring/ioc/v2bean/AppConfig.java
+++ Session5_Spring/spring-to-go/src/main/java/com/datdt/spring/ioc/v2bean/AppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 public class AppConfig {
     @Bean
-    public PdfGenenrator pdfGenerator() {
+    public PdfGenenrator thanhThao() {
         return new PdfGenenrator();
     }
 }
