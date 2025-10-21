77a7d32484c10ee17fd4d8c857ff4aae4f1b2075
dat dang <2imdanton139@gmail.com>
2025-10-21 23:56:30 +0700

    fix wrong mapping

--- Session7_SpringMVC/java-coffee/src/main/java/com/datdt/coffee/controller/web/CoffeeWebController.java
+++ Session7_SpringMVC/java-coffee/src/main/java/com/datdt/coffee/controller/web/CoffeeWebController.java
@@ -1,4 +1,11 @@
 package com.datdt.coffee.controller.web;
 
+import ch.qos.logback.core.model.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class CoffeeWebController {
+    @GetMapping("/")
+    public String home(Model model) {
+        return "index";
+    }
 }
